2dcc326ef16dd964bc50eecbb5fbcbba64708ea5
tamatsukonosuke <rao.koo621@icloud.com>
2020-02-17 20:10:49 +0900

    usermodel

--- users_manage/models.py
+++ users_manage/models.py
@@ -5,5 +5,10 @@ class User(models.Model):
     age = models.IntegerField()
     mail = models.EmailField()
 
+    def __repr__(self):
+      return "{}: {}".format(self.pk, self.name)
+
+      __str__ = __repr__  # __str__にも同じ関数を適用
+
 class Entry(models.Model):
     title = models.CharField(max_length=128)
