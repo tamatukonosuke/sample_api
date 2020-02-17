6dc2652c893aed9db4b86fab19b279d3c81c78a2
tamatsukonosuke <rao.koo621@icloud.com>
2020-02-17 20:18:23 +0900

    ageバリデーション追加

--- users_manage/models.py
+++ users_manage/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class User(models.Model):
     name = models.CharField(max_length=32)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(0)])
     mail = models.EmailField()
 
     def __repr__(self):
